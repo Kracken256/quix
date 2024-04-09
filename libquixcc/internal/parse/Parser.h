041cadbaa6f92d58bc7ea076fa53e2034a6ea122
Wesley Jones <wesjones2004@gmail.com>
2024-04-09 10:11:56 -1000

    correct license header

--- libquixcc/internal/parse/Parser.h
+++ libquixcc/internal/parse/Parser.h
@@ -24,7 +24,7 @@
 ///     Lesser General Public License for more details.                          ///
 ///                                                                              ///
 ///     You should have received a copy of the GNU Lesser General Public         ///
-///     License along with the GNU C Library; if not, see                        ///
+///     License along with the QUIX Compiler Suite; if not, see                  ///
 ///     <https://www.gnu.org/licenses/>.                                         ///
 ///                                                                              ///
 ////////////////////////////////////////////////////////////////////////////////////

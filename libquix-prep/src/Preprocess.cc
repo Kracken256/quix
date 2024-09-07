ee8fd266f1b0dc12480cc6f16a385f6f9b76531d
Wesley Jones <wesjones2004@gmail.com>
2024-09-06 22:12:09 -1000

    fix warn

--- libquix-prep/src/Preprocess.cc
+++ libquix-prep/src/Preprocess.cc
@@ -29,12 +29,58 @@
 ///                                                                          ///
 ////////////////////////////////////////////////////////////////////////////////
 
-#define __QUIX_IMPL__
-
 #include <quix-core/Error.h>
-#include <quix-lexer/Lexer.h>
+#include <quix-lexer/Token.h>
+
+#include <quix-lexer/Base.hh>
+#include <new>
 
 #include "LibMacro.h"
+
+// class Engine final {
+//   qlex_t *m_lexer;
+
+// public:
+//   Engine(FILE *file, const char *filename) {
+//     m_lexer = qlex_new(file, filename);
+//     if (!m_lexer) {
+//       qcore_panic("Engine: failed to create lexer");
+//     }
+//   }
+
+//   ~Engine() { qlex_free(m_lexer); }
+
+//   qlex_t *inner() { return m_lexer; }
+// };
+
+// #define ctx(_ptr) reinterpret_cast<Engine *>(_ptr->inner)
+
+// static qlex_tok_t _impl_peek(qlex_t *self) {
+//   /// TODO:
+
+//   // return qlex_peek(ctx(self)->inner());
+
+//   return {};
+// }
+
+// static void _impl_push(qlex_t *self, const qlex_tok_t *tok) {
+//   /// TODO:
+
+//   qlex_push(ctx(self)->inner(), *tok);
+// }
+
+// static void _impl_collect(qlex_t *self, const qlex_tok_t *tok) {
+//   /// TODO:
+
+//   qlex_collect(ctx(self)->inner(), tok);
+// }
+
+// static void _impl_destruct(qlex_t *self) {
+//   /// TODO:
+
+//   delete ctx(self);
+//   self->inner = nullptr;
+// }
 
 LIB_EXPORT qlex_t *qprep_new(FILE *file, const char *filename) {
   try {
@@ -44,10 +90,20 @@ LIB_EXPORT qlex_t *qprep_new(FILE *file, const char *filename) {
       filename = "<unknown>";
     }
 
-    /// TODO: Implement preprocessor.
-
+    // return new qlex_t{
+    //     .peek = _impl_peek,
+    //     .push = _impl_push,
+    //     .collect = _impl_collect,
+    //     .destruct = _impl_destruct,
+    //     .cur = {.ty = qErro},
+    //     .flags = QLEX_FLAG_NONE,
+    //     .filename = filename,
+    //     .inner = new Engine(file, filename),
+    // };
     return qlex_new(file, filename);
+  } catch (std::bad_alloc &) {
+    return nullptr;
   } catch (...) {
-    qcore_panic("qprep_new: failed to create preprocessor");
+    qcore_panic("qprep_new: failed to create lexer");
   }
 }

6cf0ac7be872b9fee9c81a9e914aa4a1b2590311
Wesley Jones <wesjones2004@gmail.com>
2024-09-09 00:15:55 -1000

    Private ENV

---
+++ libquix-core/include/quix-core/Env.h
@@ -0,0 +1,90 @@
+////////////////////////////////////////////////////////////////////////////////
+///                                                                          ///
+///  ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░ ░▒▓██████▓▒░  ///
+/// ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ///
+/// ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░        ///
+/// ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░      ░▒▓█▓▒░        ///
+/// ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░        ///
+/// ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ///
+///  ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░ ░▒▓██████▓▒░  ///
+///    ░▒▓█▓▒░                                                               ///
+///     ░▒▓██▓▒░                                                             ///
+///                                                                          ///
+///   * QUIX LANG COMPILER - The official compiler for the Quix language.    ///
+///   * Copyright (C) 2024 Wesley C. Jones                                   ///
+///                                                                          ///
+///   The QUIX Compiler Suite is free software; you can redistribute it or   ///
+///   modify it under the terms of the GNU Lesser General Public             ///
+///   License as published by the Free Software Foundation; either           ///
+///   version 2.1 of the License, or (at your option) any later version.     ///
+///                                                                          ///
+///   The QUIX Compiler Suite is distributed in the hope that it will be     ///
+///   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of ///
+///   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      ///
+///   Lesser General Public License for more details.                        ///
+///                                                                          ///
+///   You should have received a copy of the GNU Lesser General Public       ///
+///   License along with the QUIX Compiler Suite; if not, see                ///
+///   <https://www.gnu.org/licenses/>.                                       ///
+///                                                                          ///
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __QUIX_CORE_ENV_H__
+#define __QUIX_CORE_ENV_H__
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef uint64_t qcore_env_t;
+
+/**
+ * @brief Create a new environment.
+ * @return The environment handle.
+ */
+qcore_env_t qcore_env_create();
+
+/**
+ * @brief Drop a reference to an environment.
+ * @param env The environment handle.
+ * @note The environment will be destroyed when the last reference is dropped.
+ */
+void qcore_env_forget(qcore_env_t env);
+
+/**
+ * @brief Get the current environment.
+ * @return The current environment handle.
+ */
+qcore_env_t qcore_env_current();
+
+/**
+ * @brief Set the current environment.
+ * @param env The environment handle.
+ */
+void qcore_env_set_current(qcore_env_t env);
+
+/**
+ * @brief Get the value of an environment variable.
+ * @param key The environment variable key.
+ * @return The value of the environment variable, or NULL to unset the variable.
+ * @note Strings will be cloned internally.
+ */
+void qcore_env_set(const char *key, const char *value);
+
+#define qcore_env_unset(key) qcore_env_set(key, NULL)
+
+/**
+ * @brief Get the value of an environment variable.
+ * @param key The environment variable key.
+ * @return The value of the environment variable, or NULL if the variable is not set.
+ * @note Strings will be cloned internally.
+ */
+const char *qcore_env_get(const char *key);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __QUIX_CORE_ENV_H__
---
+++ libquix-core/src/Env.cc
@@ -0,0 +1,84 @@
+////////////////////////////////////////////////////////////////////////////////
+///                                                                          ///
+///  ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░ ░▒▓██████▓▒░  ///
+/// ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ///
+/// ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░        ///
+/// ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░      ░▒▓█▓▒░        ///
+/// ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░        ///
+/// ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ///
+///  ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░ ░▒▓██████▓▒░  ///
+///    ░▒▓█▓▒░                                                               ///
+///     ░▒▓██▓▒░                                                             ///
+///                                                                          ///
+///   * QUIX LANG COMPILER - The official compiler for the Quix language.    ///
+///   * Copyright (C) 2024 Wesley C. Jones                                   ///
+///                                                                          ///
+///   The QUIX Compiler Suite is free software; you can redistribute it or   ///
+///   modify it under the terms of the GNU Lesser General Public             ///
+///   License as published by the Free Software Foundation; either           ///
+///   version 2.1 of the License, or (at your option) any later version.     ///
+///                                                                          ///
+///   The QUIX Compiler Suite is distributed in the hope that it will be     ///
+///   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of ///
+///   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      ///
+///   Lesser General Public License for more details.                        ///
+///                                                                          ///
+///   You should have received a copy of the GNU Lesser General Public       ///
+///   License along with the QUIX Compiler Suite; if not, see                ///
+///   <https://www.gnu.org/licenses/>.                                       ///
+///                                                                          ///
+////////////////////////////////////////////////////////////////////////////////
+
+#include <quix-core/Env.h>
+#include <quix-core/Error.h>
+
+#include <atomic>
+#include <string>
+#include <unordered_map>
+
+#include "LibMacro.h"
+
+static std::atomic<qcore_env_t> g_last_env = 1;
+static std::unordered_map<qcore_env_t, std::unordered_map<std::string, std::string>> g_envs;
+static thread_local qcore_env_t g_current_env = 0;
+
+LIB_EXPORT qcore_env_t qcore_env_create() {
+  qcore_env_t env = g_last_env++;
+  g_envs[env] = std::unordered_map<std::string, std::string>();
+  return env;
+}
+
+LIB_EXPORT void qcore_env_destroy(qcore_env_t env) {
+  qcore_assert(g_envs.count(env), "Environment does not exist.");
+  g_envs.erase(env);
+}
+
+LIB_EXPORT qcore_env_t qcore_env_current() {
+  qcore_assert(g_envs.count(g_current_env), "Current environment does not exist.");
+  return g_current_env;
+}
+
+LIB_EXPORT void qcore_env_set_current(qcore_env_t env) {
+  qcore_assert(g_envs.count(env), "Environment does not exist.");
+  g_current_env = env;
+}
+
+LIB_EXPORT void qcore_env_set(const char *key, const char *value) {
+  qcore_assert(g_envs.count(g_current_env), "Current environment does not exist.");
+
+  if (value == NULL) {
+    g_envs[g_current_env].erase(key);
+  } else {
+    g_envs[g_current_env][key] = value;
+  }
+}
+
+LIB_EXPORT const char *qcore_env_get(const char *key) {
+  qcore_assert(g_envs.count(g_current_env), "Current environment does not exist.");
+
+  if (g_envs[g_current_env].count(key)) {
+    return g_envs[g_current_env][key].c_str();
+  } else {
+    return NULL;
+  }
+}
